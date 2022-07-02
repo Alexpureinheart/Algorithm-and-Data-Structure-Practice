40eaeb8ac5fc82435b57f6f418c6f501b50e732e
Alex Watts-Barnett <alexrwb@gmail.com>
2022-07-03 01:41:00 +0900

    successfully built a working stack, will refactor

--- stack/stack.py
+++ stack/stack.py
@@ -17,6 +17,7 @@ class Node():
 
 
 class Linked_list():
+
     def __init__(self, value):
         self.head_node = Node(value)
 
@@ -66,3 +67,75 @@ class Linked_list():
                 string_list += str(current_node.get_value()) + "\n"
             current_node = current_node.get_next_node()
         return string_list
+
+
+class Stack():
+    def __init__(self, max_size = None):
+        self.top_value = None
+        self.size = 0
+        self.max_size = max_size
+
+    def push(self, value):
+        if self.size == self.max_size:
+            return(print("Stack full!"))
+        else:
+            new_node = Node(value)
+            new_node.set_next_node(self.top_value)
+            self.top_value = new_node
+            self.size += 1
+        
+    def pop(self):
+        if self.size == 0:
+            return(print("Stack empty!"))
+        elif self.size == 1: 
+            to_be_removed = self.top_value
+            self.top_value = None
+            self.size -= 1
+            return to_be_removed
+        else:
+            to_be_removed = self.top_value
+            self.top_value = to_be_removed.get_next_node()
+            to_be_removed.next_node = None
+            self.size -= 1
+            return to_be_removed
+    
+
+    def peek(self):
+        if self.size == 0:
+            return(print("Stack Empty!"))
+        else:
+            return self.top_value.get_value() + "\n"
+
+my_stack = Stack(5)
+my_stack.push("Green eggs and ham")
+print(my_stack.peek())
+my_stack.push("Hop on pop")
+print(my_stack.peek())
+my_stack.push("The Lorax")
+print(my_stack.peek())
+my_stack.push("The Bread and Butter Battle")
+print(my_stack.peek())
+my_stack.push("The letters after Z")
+print(my_stack.peek())
+my_stack.push("Yurtle the Turtle")
+print(my_stack.peek())
+
+my_stack.pop()
+print(my_stack.peek())
+my_stack.pop()
+print(my_stack.peek())
+my_stack.pop()
+print(my_stack.peek())
+my_stack.pop()
+print(my_stack.peek())
+my_stack.pop()
+print(my_stack.peek())
+
+
+
+            
+            
+        
+
+        
+        
