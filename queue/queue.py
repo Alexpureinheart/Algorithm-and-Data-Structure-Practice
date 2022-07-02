49d0b0aee228135fae8d45fb34d6218771dfe254
Alex Watts-Barnett <alexrwb@gmail.com>
2022-07-03 00:08:32 +0900

    changed qeueu to be bounded

--- queue/queue.py
+++ queue/queue.py
@@ -74,35 +74,52 @@ class Linked_list():
 #add max_size attribute to make bounded 
 
 class Queue():
-    def __init__(self, value):
-        self.first_node = Node(value)
+    def __init__(self, value, max_size = None):
+        self.head = None
+        self.tail = None
+        self.max_size = max_size
+        self.size = 0
     
     def enqueue(self, value):
         new_node = Node(value)
-        new_node.set_next_node(self.first_node)
-        self.first_node = new_node
+        if self.size == self.max_size:
+            return print("No more room in queue.")
+        elif self.size == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.set_next_node(self.head)
+            self.head = new_node
+        self.size += 1
+        
+        
 
     def dequeue(self):
-        current_node = self.first_node
-        previous_node = None
+        if self.size == 0:
+            return print("Queue is empty, nothing to dequeue.")
+        else:
+            current_node = self.head
+            previous_node = None
         while current_node.next_node:
             previous_node = current_node
             current_node = current_node.get_next_node()
         print(current_node.get_value())
         if previous_node == None:
-            self.first_node = None
+            self.head = None
         elif current_node.get_next_node == None:
             previous_node.set_next_node(None)
+        self.size -= 1
+        return(current_node.get_value())
 
     def peek(self):
-        if self.first_node == None:
+        if self.size == 0:
             return print("Queue empty.")
-        current_node = self.first_node
+        current_node = self.head
         if current_node.get_next_node() != None:
             while current_node.next_node:
                 current_node = current_node.get_next_node()
-        print(current_node.get_value())
-        return(current_node.get_value())
+        return print(current_node.get_value())
+        
 
 my_queue = Queue("Bacon")
 my_queue.peek()
