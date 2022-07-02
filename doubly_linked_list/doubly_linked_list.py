781875c8d4c966671cc3a9979c85ad7db64ac649
Alex Watts-Barnett <alexrwb@gmail.com>
2022-07-02 16:50:22 +0900

    Fixed add_at_head and added add_at_tail

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -22,9 +22,13 @@ class Node():
 class Doubly_linked_list():
     def __init__(self, value):
         self.head_node = Node(value)
+        self.tail_node = self.head_node
 
     def get_head_node(self):
         return self.head_node
+
+    def get_tail_node(self):
+        return self.tail_node
 
     def add_to_head(self, value):
         new_node = Node(value)
@@ -34,17 +38,30 @@ class Doubly_linked_list():
             self.head_node.set_previous_node(new_node)
             new_node.set_next_node(self.get_head_node())
             self.head_node = new_node
+    
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.get_tail_node() == None:
+            self.tail_node = new_node
+        else: 
+            self.tail_node.set_next_node(new_node)
+            new_node.set_previous_node(self.get_tail_node())
+            self.tail_node = new_node
 
     def print_list(self):
         current_node = self.get_head_node()
         while current_node.next_node:
             print(current_node.get_value())
             current_node = current_node.next_node
+        print(current_node.get_value())
             
 
 my_doubly = Doubly_linked_list("paper")
 my_doubly.add_to_head("pen")
 my_doubly.add_to_head("pencil")
+
+my_doubly.add_to_tail("eraser")
+my_doubly.add_to_tail("chalk")
 my_doubly.print_list()
 
             
