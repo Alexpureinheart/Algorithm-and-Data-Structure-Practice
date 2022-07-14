1b957a67d0a4860bbf41c33eda9efbd48bcf8e2d
Alexrwb <alexrwb@gmail.com>
2022-07-14 09:33:15 +0900

    added open adressing collision handling to assign method

--- hash_map/hash_map_DIY.py
+++ hash_map/hash_map_DIY.py
@@ -1,3 +1,5 @@
+#this is a HashMap using open adresing for collision handling 
+
 class HashMap:
     def __init__(self, array_length):
         self.array_length = array_length
@@ -12,18 +14,33 @@ class HashMap:
         return hash_code % self.array_length
         
 
-    def assign(self, key, value):
+    def assign(self, key, value, collisions = 0):
         array_index = self.compress(self.hash(key))
         if self.array[array_index] == None: 
-            self.array[array_index] = [value]
+            self.array[array_index] = [key, value]
 
         if self.array[array_index] == key:
-            self.array[array_index] = [value]
+            self.array[array_index] = [key, value]
 
+        collisions = 1
+
+        while(self.array[array_index] != key):
+            new_array_index = self.compress(self.hash(key, collisions))
             
+            if self.array[new_array_index][0] == key:
+                self.array[new_array_index] = [key, value]
+                return 
+
+            if self.array[new_array_index] == None:
+                self.array[new_array_index] = [key, value]
+                return
+            
+            collisions += 1 
 
     def retrieve(self, key):
         array_index = self.compress(self.hash(key))
+        possible_return_value = self.array[array_index]
+
         if self.array[array_index] == None:
             return None
 
