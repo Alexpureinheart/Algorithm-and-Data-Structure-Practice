612096e402841790a3d68b26cc06765db5f4b315
Alexrwb <alexrwb@gmail.com>
2022-07-14 09:42:48 +0900

    added open adressing collsion handling to retrieve method

--- hash_map/hash_map_DIY.py
+++ hash_map/hash_map_DIY.py
@@ -1,5 +1,8 @@
 #this is a HashMap using open adresing for collision handling 
 
+from turtle import pos
+
+
 class HashMap:
     def __init__(self, array_length):
         self.array_length = array_length
@@ -41,11 +44,26 @@ class HashMap:
         array_index = self.compress(self.hash(key))
         possible_return_value = self.array[array_index]
 
-        if self.array[array_index] == None:
+        if possible_return_value == None:
             return None
 
-        if self.array[array_index] != None:
+        if possible_return_value[0] == key:
             return str(self.array[array_index][0])
+
+        retrieval_collisions = 1
+
+        while possible_return_value != key:
+            retrieval_array_index = self.compress(self.hash(key, retrieval_collisions))
+            possible_return_value = self.array[retrieval_array_index]
+
+            if possible_return_value == None:
+                return None
+
+            if possible_return_value[0] != key:
+                retrieval_collisions += 1
+
+            if possible_return_value[0] == key:
+                return possible_return_value[1] 
 
 my_hash_map = HashMap(15)
 my_hash_map.assign("Panda Bear", "Norman")
